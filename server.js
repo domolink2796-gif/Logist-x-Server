124226d48e51909c19a7dd1b4d74ebe969e1eae3
domolink2796-gif <domolink_2796@mail.ru>
2025-12-29 08:23:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 
 // --- НАСТРОЙКИ ---
-const TOKEN = '7908672389:AAFqJsmCmlJHSckewNPue_XVa_WTxKY7-Aw';
+const TOKEN = '7908672389:AAFqJsmCmlJHSckewNPue_XVa_w';
 const CLIENT_ID = '355201275272-14gol1u31gr3qlan5236v241jbe13r0a.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
 const REFRESH_TOKEN = '1//04Xx4TeSGvK3OCgYIARAAGAQSNwF-L9Irgd6A14PB5ziFVjs-PftE7jdGY0KoRJnXeVlDuD1eU2ws6Kc1gdlmSYz99MlOQvSeLZ0';
